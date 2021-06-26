ccb1c6c0b888db86c811c74b710442f03dd75254
Simon Crowe <simon.r.crowe@protonmail.com>
2021-06-26 08:19:06 +0100

    Add screenlayout for 15", use hosts for dirnames

---
+++ screenlayout/drone/2x24+1x15.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1-0.1 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-1-0.2 --mode 1920x1080 --pos 3000x367 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off
